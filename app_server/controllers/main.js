0f3e8564981c6a0c21e9a22b5ee0545c86580a8c
Bryan Lackey <bryanlackey@Pro.local>
2015-06-27 16:24:52 -0400

    added locations and others controllers and routes

--- app_server/controllers/main.js
+++
@@ -1,4 +0,0 @@
-/* GET Home page */
-module.exports.index = function(req, res) {
-  res.render('index', { title: 'Express' });
-};
---
+++ app_server/controllers/others.js
@@ -0,0 +1,4 @@
+/* GET About page */
+module.exports.about = function(req, res) {
+  res.render('index', { title: 'About' });
+};
