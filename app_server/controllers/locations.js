17a5c4c9e293e3cc43cf327fa6c141ff54fc6e46
Bryan Lackey <bryanlackey@Pro.local>
2015-06-27 17:27:05 -0400

    Added location-info page

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,11 +1,11 @@
 /* GET 'home' page */
 module.exports.homeList = function(req, res) {
-  res.render('index', { title: 'Home' });
+  res.render('locations-list', { title: 'Home' });
 };
 
 /* GET 'Location info' page */
 module.exports.locationInfo = function(req, res) {
-  res.render('index', { title: 'Location info' });
+  res.render('location-info', { title: 'Location info' });
 };
 
 /* GET 'Add review' page */
