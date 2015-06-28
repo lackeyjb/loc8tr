67dab0f9cb790c80cd9607a08309b237ef5a6187
Bryan Lackey <bryanlackey@Pro.local>
2015-06-28 01:07:36 -0400

    troubleshooted db.js

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,47 +1,49 @@
 var mongoose = require('mongoose');
+var gracefulShutdown;
 var dbURI = 'mongodb://localhost/Loc8r';
 if (process.env.NODE_ENV === 'production') {
-  dbURI = process.env.MONGOLAB_URI;
+    dbURI = process.env.MONGOLAB_URI;
 }
+
 mongoose.connect(dbURI);
 
+// CONNECTION EVENTS
 mongoose.connection.on('connected', function() {
-  console.log('Mongoose connected to '+ dbURI);
+    console.log('Mongoose connected to ' + dbURI);
 });
 mongoose.connection.on('error', function(err) {
-  console.log('Mongoose connection error: ' + err);
+    console.log('Mongoose connection error: ' + err);
 });
 mongoose.connection.on('disconnected', function() {
-  console.log('Mongoose disconnected');
+    console.log('Mongoose disconnected');
 });
 
-// Capture app termination / restart events
-var gracefulShutdown = function(msg, callback) {
-  mongoose.connection.close(function() {
-    console.log('Mongoose disconnected through ' + msg);
-    callback();
-  });
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+gracefulShutdown = function(msg, callback) {
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
 };
-
-// for nodemon restarts
+// For nodemon restarts
 process.once('SIGUSR2', function() {
-  gracefulShutdown('nodemon restart', function() {
-    process.kill(process.pid, 'SIGUSR2');
-  });
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
 });
-
-// for app termination
+// For app termination
 process.on('SIGINT', function() {
-  gracefulShutdown('app termination', function() {
-    process.exit(0);
-  });
+    gracefulShutdown('app termination', function() {
+        process.exit(0);
+    });
 });
-
-// for Heroku app termination
+// For Heroku app termination
 process.on('SIGTERM', function() {
-  gracefulShutdown('Heroku app shutdown', function(){
-    process.exit(0);
-  });
+    gracefulShutdown('Heroku app termination', function() {
+        process.exit(0);
+    });
 });
 
+// BRING IN YOUR SCHEMAS & MODELS
 require('./locations');
