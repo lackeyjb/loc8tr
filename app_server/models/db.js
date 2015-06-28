4d7af32683736cb5d838f9ca54b767ee2e479575
Bryan Lackey <bryanlackey@Pro.local>
2015-06-28 00:39:20 -0400

    added models and mongodb

---
+++ app_server/models/db.js
@@ -0,0 +1,45 @@
+var mongoose = require('mongoose');
+var dbURI = 'mongodb://localhost/Loc8r';
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function() {
+  console.log('Mongoose connected to '+ dbURI);
+});
+mongoose.connection.on('error', function(err) {
+  console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function() {
+  console.log('Mongoose disconnected');
+});
+
+// Capture app termination / restart events
+var gracefulShutdown = function(msg, callback) {
+  mongoose.connection.close(function() {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+};
+
+// for nodemon restarts
+process.once('SIGUSR2', function() {
+  gracefulShutdown('nodemon restart', function() {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+
+// for app termination
+process.on('SIGINT', function() {
+  gracefulShutdown('app termination', function() {
+    process.exit(0);
+  });
+});
+
+// for Heroku app termination
+process.on('SIGTERM', function() {
+  gracefulShutdown('Heroku app shutdown', function(){
+    process.exit(0);
+  });
+});
+
+require('./locations');
