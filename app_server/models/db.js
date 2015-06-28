e6c6df71245189ea5cc5b5f2456befd17154e489
Bryan Lackey <bryanlackey@Pro.local>
2015-06-28 00:56:16 -0400

    added production db for heroku

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var dbURI = 'mongodb://localhost/Loc8r';
-
+if (process.env.NODE_ENV === 'production') {
+  dbURI = process.env.MONGOLAB_URI;
+}
 mongoose.connect(dbURI);
 
 mongoose.connection.on('connected', function() {
