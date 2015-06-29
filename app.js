7d4b17ccca23f308e4cb76492e77021e244bcb58
Bryan Lackey <bryanlackey@Pro.attlocal.net>
2015-06-28 22:10:04 -0400

    added create reviews

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-require('./app_server/models/db');
+require('./app_api/models/db');
 
 var routes = require('./app_server/routes/index');
-var users = require('./app_server/routes/users');
+var routesApi = require('./app_api/routes/index');
+// var users = require('./app_server/routes/users');
 
 var app = express();
 
@@ -24,7 +25,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api', routesApi);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
