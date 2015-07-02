dd4f766965fe9715cf304912e68234085f375c1f
Bryan Lackey <bryanlackey@Pro.attlocal.net>
2015-07-01 21:14:32 -0400

    added location view with api call

--- app_api/models/locations.js
+++ app_api/models/locations.js
@@ -2,24 +2,50 @@ var mongoose = require('mongoose');
 
 var reviewSchema = new mongoose.Schema({
   author: String,
-  rating: {type: Number, required: true, min: 0, max: 5},
+  rating: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 5
+  },
   reviewText: String,
-  createdOn: {type: Date, "default": Date.now}
+  createdOn: {
+    type: Date,
+    "default": Date.now
+  }
 });
 
 var openingTimeSchema = new mongoose.Schema({
-  days: {type: String, required: true},
+  days: {
+    type: String,
+    required: true
+  },
   opening: String,
   closing: String,
-  closed: {type: Boolean, required: true}
+  closed: {
+    type: Boolean,
+    required: true
+  }
 });
 
 var locationSchema = new mongoose.Schema({
-  name: {type: String, required: true},
+  name: {
+    type: String,
+    required: true
+  },
   address: String,
-  rating: {type: Number, "default": 0, min: 0, max: 5},
+  rating: {
+    type: Number,
+    "default": 0,
+    min: 0,
+    max: 5
+  },
   facilities: [String],
-  coords: {type: [Number], index: '2dsphere', required: true},
+  coords: {
+    type: [Number],
+    index: '2dsphere',
+    required: true
+  },
   openingTimes: [openingTimeSchema],
   reviews: [reviewSchema]
 });
--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -18,7 +18,32 @@ var _formatDistance = function(distance) {
     return numDistance + unit;
 };
 
+var _showError = function(req, res, status) {
+  var title, content;
+  if (status === 404) {
+    title = '404, page not found';
+    content = 'Oh dear. Looks like we can\'t find this page. Sorry.';
+  } else {
+    title = status + ', something\'s gone wrong';
+    content = 'Something, somewhere, has gone just a little bit wrong.';
+  }
+  res.status(status);
+  res.render('generic-text', {
+    title: title,
+    content: content
+  });
+};
+
 var renderHomepage = function(req, res, responseBody) {
+  var message;
+  if (!(responseBody instanceof Array)) {
+    message = 'API lookup error';
+    responseBody = [];
+  } else {
+    if (!responseBody.length) {
+      message = 'No places found nearby';
+    }
+  }
   res.render('locations-list', {
     title: 'Loc8r - find a place to work with wifi',
     pageHeader: {
@@ -26,7 +51,8 @@ var renderHomepage = function(req, res, responseBody) {
       strapline: 'Find places to work with wifi near you!'
     },
     sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or       a pint? Let Loc8r help you find the place you're looking for.",
-    locations: responseBody
+    locations: responseBody,
+    message: message
   });
 };
 
@@ -49,56 +75,52 @@ module.exports.homeList = function(req, res) {
     function(err, response, body) {
       var i, data;
       data = body;
-      for (i=0; i<data.length; i++) {
-        data[i].distance = _formatDistance(data[i].distance);
+      if (response.statusCode === 200 && data.length) {
+        for (i=0; i<data.length; i++) {
+          data[i].distance = _formatDistance(data[i].distance);
+        }
       }
       renderHomepage(req, res, data);
     }
   );
 };
 
-/* GET 'Location info' page */
-module.exports.locationInfo = function(req, res) {
+var renderDetailPage = function(req, res, locDetail) {
   res.render('location-info', {
-    title: 'Starcups',
-    pageHeader: {title: 'Starcups'},
+    title: locDetail.name,
+    pageHeader: {title: locDetail.name},
     sidebar: {
       context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
-      callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+      callToAction: "If you've been and you like it - or if you don't - please leave a review to help other people just like you."
     },
-    location: {
-      name: 'Starcups',
-      address: '125 High Street, Reading, RG6 1PS',
-      rating: 3,
-      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-      coords: {lat: 51.455041, lng: -0.9690884},
-      openingTimes: [{
-        days: 'Monday - Friday',
-        opening: '7:00am',
-        closing: '7:00pm',
-        closed: false
-      }, {
-        days: 'Saturday',
-        opening: '8:00am',
-        closing: '5:00pm',
-        closed: false
-      }, {
-        days: 'Sunday',
-        closed: true
-      }],
-      reviews: [{
-        author: 'Bryan Lackey',
-        rating: 5,
-        timestamp: '16 July 2014',
-        reviewText: 'What a great place. I can\'t say enough good things about it.'
-      }, {
-        author: 'Charlie Chaplin',
-        rating: 3,
-        timestamp: '16 July 2014',
-        reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
-      }]
-    }
+    location: locDetail
   });
+};
+
+/* GET 'Location info' page */
+module.exports.locationInfo = function(req, res) {
+  var requestOptions, path;
+  path = '/api/locations/' + req.params.locationid;
+  requestOptions = {
+    url: apiOptions.server + path,
+    method: 'GET',
+    json: {}
+  };
+  request(
+    requestOptions,
+    function(err, response, body) {
+      var data = body;
+      if (response.statusCode === 200) {
+        data.coords = {
+          lng: body.coords[0],
+          lat: body.coords[1]
+        };
+        renderDetailPage(req, res, data);
+      } else {
+        _showError(req, res, response.statusCode);
+      }
+    }
+  );
 };
 
 /* GET 'Add review' page */
