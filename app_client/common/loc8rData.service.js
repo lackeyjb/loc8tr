89071d21656d1f92c3b3633c4fe9a9c2999d5454
Bryan Lackey <bryanlackey@Pro.attlocal.net>
2015-07-03 22:15:29 -0400

    added app_client

---
+++ app_client/common/loc8rData.service.js
@@ -0,0 +1,13 @@
+angular
+  .module('loc8rApp')
+  .service('loc8rData', loc8rData);
+
+function loc8rData ($http) {
+  var locationByCoords = function (lat, lng) {
+    return $http.get('/api/locations?lng=' + lng + '&lat=' + lat +
+      '&maxDistance=20');
+  };
+  return {
+    locationByCoords: locationByCoords
+  };
+}
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,12 +4,12 @@ var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 
 /* Locations pages */
-router.get('/', ctrlLocations.homeList);
-router.get('/location/:locationid', ctrlLocations.locationInfo);
-router.get('/location/:locationid/reviews/new', ctrlLocations.addReview);
-router.post('/location/:locationid/reviews/new', ctrlLocations.doAddReview);
+router.get('/', ctrlOthers.angularApp);
+// router.get('/location/:locationid', ctrlLocations.locationInfo);
+// router.get('/location/:locationid/reviews/new', ctrlLocations.addReview);
+// router.post('/location/:locationid/reviews/new', ctrlLocations.doAddReview);
 
 /* Other pages */
-router.get('/about', ctrlOthers.about);
+// router.get('/about', ctrlOthers.about);
 
 module.exports = router;
